fe6bcec61911820b74eaeb6df0b52083acf60d66
Ranjeetha Virdi <81987445+ranjeetha-virdi@users.noreply.github.com>
2023-04-28 07:58:53 +0200

    Create main.tf

---
+++ lab-13/main.tf
@@ -0,0 +1,126 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+
+data "aws_availability_zones" "working" {}
+
+data "aws_ami" "latest_amazonlinux" {
+  owners      = ["137112412989"]
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
+
+resource "aws_security_group" "web" {
+  name = "Web security group"
+  dynamic "ingress" {
+    for_each = ["80", "443"]
+
+    content {
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+
+    }
+  }
+  egress {
+    description = "allow all ports"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+  tags = {
+    name  = "Web_security_group"
+    owner = "Ranjeetha R"
+  }
+}
+
+resource "aws_launch_configuration" "web" {
+  name_prefix     = "Webserver-highly-available-LC-"
+  image_id        = data.aws_ami.latest_amazonlinux.id
+  instance_type   = "t3.micro"
+  security_groups = [aws_security_group.web.id]
+  user_data       = file("user_data.sh")
+}
+
+resource "aws_autoscaling_group" "web" {
+  name                 = "ASG-${aws_launch_configuration.web.name}"
+  launch_configuration = aws_launch_configuration.web.name
+  min_size             = 3
+  max_size             = 3
+  min_elb_capacity     = 3
+  health_check_type    = "ELB"
+  vpc_zone_identifier  = [aws_default_subnet.default_az1.id, aws_default_subnet.default_az2.id]
+  load_balancers       = [aws_elb.web.name]
+
+  dynamic "tag" {
+    for_each = {
+      Name   = "WebServer in ASG"
+      owner  = "Ranjeetha R"
+      TAGKEY = "TAGVALUE"
+    }
+
+    content {
+      key                 = tag.key
+      value               = tag.value
+      propagate_at_launch = true
+
+    }
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+}
+
+
+resource "aws_elb" "web" {
+  name               = "Webserver-Highleavailable-ELB"
+  availability_zones = [data.aws_availability_zones.working.names[0], data.aws_availability_zones.working.names[1]]
+  security_groups    = [aws_security_group.web.id]
+  listener {
+
+    lb_port           = 80
+    lb_protocol       = "http"
+    instance_port     = 80
+    instance_protocol = "http"
+
+  }
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    target              = "HTTP:80/"
+    interval            = 10
+  }
+
+  tags = {
+    name  = "Highlyavailable_Webserver_ELB"
+    owner = "Ranjeetha R"
+  }
+}
+
+resource "aws_default_subnet" "default_az1" {
+  availability_zone = data.aws_availability_zones.working.names[0]
+}
+
+
+resource "aws_default_subnet" "default_az2" {
+  availability_zone = data.aws_availability_zones.working.names[1]
+}
+
+
+output "web_loadbalncer_url" {
+  value = aws_elb.web.dns_name
+}
+
+
+
