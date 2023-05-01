7450a08c97547d2c76c33bdd1e804dc7a11dbc7b
Ranjeetha Virdi <81987445+ranjeetha-virdi@users.noreply.github.com>
2023-05-01 19:24:43 +0200

    Add files via upload

---
+++ lab-16/variables.tf
@@ -0,0 +1,20 @@
+variable "enviornment" {
+  default = "DEV"
+}
+
+variable "project_name" {
+  default = "Cremson"
+}
+
+variable "owner" {
+
+  default = "RR"
+}
+
+variable "tags" {
+
+  default = {
+    budget_code = 1232323
+    manager     = "PR"
+  }
+}
