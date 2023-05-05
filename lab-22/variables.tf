cb0debd1afaadbd701953ce28f22683bc1b05ba8
Ranjeetha Virdi <81987445+ranjeetha-virdi@users.noreply.github.com>
2023-05-05 12:36:23 +0200

    Loop Construct: for x in xyz

---
+++ lab-22/variables.tf
@@ -0,0 +1,18 @@
+variable "aws_users" {
+  description = "The AWS user names"
+  default = [
+    "RR@Virdi.net",
+    "Preet@Virdi.net",
+    "Preetu@Virdi.net",
+    "Ran@Virdi.net",
+    "PR@Virdi.net",
+    "Lalita@Virdi.net",
+    "Ravi@Virdi.net",
+    "Lat@Virdi.net",
+    "Mat",
+    "Kat",
+    "Pat",
+    "Loni",
+    "Miyo"
+  ]
+}
